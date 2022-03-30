cf7ce852ef05bc28180c0cd5956f199e8e77210b
botul <pawel.botulinski@gmail.com>
2022-03-30 20:19:53 +0200

    womens day function

---
+++ day_08.py
@@ -0,0 +1,8 @@
+def womensday(name: str) -> str:
+    mens_name = []
+    women_name = []
+    if name in women_name or [-1] == 'a' and name not in mens_name:
+        return "Wszystkiego najlepszego w dniu kobiet!"
+    else:
+        return f"Jesteś super {name}!"
+
